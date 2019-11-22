0a6e14f54173889d25393f8bcf9d0cb77f34ae78
rpulkka <dreamspace2017@yandex.ru>
2019-11-22 12:29:30 +0200

    Created ItemsDAO.

---
+++ src/main/java/dataAccess/ItemsDAO.java
@@ -0,0 +1,20 @@
+
+package dataAccess;
+
+import domain.Item;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsDAO implements ItemDAO {
+
+    private ArrayList<Item> items;
+    
+    public void create(Object item) {
+        Item newItem = (Item)item;
+        items.add(newItem);
+    }
+
+    public List list() {
+        return items;
+    }
+}
