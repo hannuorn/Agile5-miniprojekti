829fba1e8524ec8413e262b09a8bcf4da038582e
Roeoeri <tomi.haapakoski@helsinki.fi>
2019-11-22 11:00:16 +0200

    Dependicies for spark added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import domain.Book;
+import spark.ModelAndView;
+import static spark.Spark.*;
+import spark.template.velocity.VelocityTemplateEngine;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Vinkkikirjasto");
+        Book book = new Book("Matti Luukkainen", "Ohjelmistotuonto", "1111BBBBSSSS", "Ohtu", "Testi");
+        System.out.println(book.getInfo());
+        get("/hello", (req, res) -> book.getInfo());
+            
+        
+        
+        
+       
     }
 }
