edf4cd09c2919e03389c9aa95c8ff1075c9e9dd4
Hannu Örn <hannu.orn@iki.fi>
2019-11-27 23:42:46 +0200

    Heroku config, HTTP port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,21 @@ import static spark.Spark.*;
 
 public class Main {
     
-    static String LAYOUT = "templates/layout.html";
-    
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
         staticFiles.location("/public");
         ItemDAO dao = new ItemDAO();
         HexadecimalGenerator generator = new HexadecimalGenerator();
