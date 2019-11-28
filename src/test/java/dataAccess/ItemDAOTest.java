79b25c4ff0ba995cccb0e6f5ac1ac2320eeaf764
Hannu Örn <hannu.orn@iki.fi>
2019-11-28 11:27:02 +0200

    Enable ItemDAOTest again

--- src/test/java/dataAccess/ItemDAOTest.java
+++ src/test/java/dataAccess/ItemDAOTest.java
@@ -1,16 +1,16 @@
 package dataAccess;
 
-import dataAccess.SQLItemDAO;
+import dataAccess.MemoryItemDAO;
 import domain.Book;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ItemDAOTest {
     
-//    @Test
+    @Test
     public void createAddsBookToList() {
         
-        SQLItemDAO dao = new SQLItemDAO();
+        MemoryItemDAO dao = new MemoryItemDAO();
         dao.create(new Book("Robert Martin", "Clean Code: A Handbook of Agile Software Craftsmanship", "978-0132350884", "Ohjelmointi, design patterns", "TKT20006 Ohjelmistotuotanto"));
         
         assertEquals(1, dao.list().size());
